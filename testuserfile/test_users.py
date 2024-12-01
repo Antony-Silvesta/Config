c7a66f5802cd5968a9667135881a16758703e215
Antony-Silvesta <antonysilvesta74@gmail.com>
2024-12-01 22:32:08 +0530

    Updated 3

--- testuserfile/test_users.py
+++ testuserfile/test_users.py
@@ -1,5 +1,5 @@
 def test_valid_users(mongodb_compass_seed):
-    db = mongodb_compass_seed  # Connect to the seeded MongoDB database
+    db = mongodb_compass_seed  # Connect to the seeded MongoDB db
     valid_users = list(db["users"].find({"is_valid": True}))
     assert len(valid_users) == 1, "Expected 1 valid users."
 
