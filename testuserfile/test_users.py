514551a60911ce3e1b310f8a0b02de8dc6738f32
Antony-Silvesta <antonysilvesta74@gmail.com>
2024-12-01 22:30:25 +0530

    Updated 3

--- testuserfile/test_users.py
+++ testuserfile/test_users.py
@@ -1,9 +1,9 @@
 def test_valid_users(mongodb_compass_seed):
     db = mongodb_compass_seed  # Connect to the seeded MongoDB database
     valid_users = list(db["users"].find({"is_valid": True}))
-    assert len(valid_users) == 2, "Expected 2 valid users."
+    assert len(valid_users) == 1, "Expected 1 valid users."
 
 def test_invalid_users(mongodb_compass_seed):
     db = mongodb_compass_seed
     invalid_users = list(db["users"].find({"is_valid": False}))
-    assert len(invalid_users) == 2, "Expected 2 invalid users."
+    assert len(invalid_users) == 1, "Expected 1 invalid users."
